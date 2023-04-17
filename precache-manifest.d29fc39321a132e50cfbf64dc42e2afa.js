ffc54bf9e56a91bc20fe88649b6795c3706aa88d
Habiba Hossam <habibahossam.hh@gmail.com>
2023-04-17 08:42:31 +0200

    Updates

---
+++ precache-manifest.d29fc39321a132e50cfbf64dc42e2afa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "833503e77514788707b0",
+    "url": "/grad_project.git/static/css/main.2525c977.chunk.css"
+  },
+  {
+    "revision": "833503e77514788707b0",
+    "url": "/grad_project.git/static/js/main.833503e7.chunk.js"
+  },
+  {
+    "revision": "b238e661e0ea51e3a138",
+    "url": "/grad_project.git/static/js/1.b238e661.chunk.js"
+  },
+  {
+    "revision": "09b0490c38e5146f8650",
+    "url": "/grad_project.git/static/js/runtime~main.09b0490c.js"
+  },
+  {
+    "revision": "559a7711cdf492912c87a72e2bb54e37",
+    "url": "/grad_project.git/static/media/logo.559a7711.png"
+  },
+  {
+    "revision": "78a4fa36001cbc79bcb8033d50a3d9cc",
+    "url": "/grad_project.git/static/media/firstInfo.78a4fa36.jpeg"
+  },
+  {
+    "revision": "26856308ed52d5ef8646595b119f802e",
+    "url": "/grad_project.git/static/media/firstSlider.26856308.jpg"
+  },
+  {
+    "revision": "215e87f3fc6ad73b5c2186c12a817c98",
+    "url": "/grad_project.git/static/media/secInfo.215e87f3.jpg"
+  },
+  {
+    "revision": "22628be9f2dc6b3574b9aed2bf314274",
+    "url": "/grad_project.git/static/media/mainPic.22628be9.png"
+  },
+  {
+    "revision": "ae13f84b85f1a19c84aadf1ca1d17832",
+    "url": "/grad_project.git/index.html"
+  }
+];
